e3ae878ac9d80b4dd2bb2f10aae6a97cc66e39cb
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-08 22:59:06 -0500

    Fully functioning text editor...all the data is saved on refresh and also when closing out of a browser and returning as long as the server is still running. database has been update and configured as well.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,33 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jateDB", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jateStore")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jateStore", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB("jateDB", 1);
+  const dbt = db.transaction("jateStore", "readwrite");
+  const store = dbt.objectStore("jateStore");
+  const result = await store.put({ id: 1, value: content });
+  console.log("Content has been added to the database", result.value);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB("jateDB", 1);
+  const dbt = db.transaction("jateStore", "readonly");
+  const store = dbt.objectStore("jateStore");
+  const text = await store.get(1);
+  await dbt.done;
+  console.log("Content has been sent from database", text);
+  return text?.value;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,60 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const is_prod = (process.env.NODE_ENV || "").trim() === "production";
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./index.html",
+    filename: "index.html",
+  }),
+];
+
+if (is_prod) {
+  plugins.push(
+    ...[
+      new WebpackPwaManifest({
+        name: "Text Editor",
+        short_name: "Txt Ed",
+        description:
+          "A text editor PWA (progressive web app) that can work both online and offline",
+        background_color: "#ffffff",
+        theme_color: "#000",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+    ]
+  );
+}
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: is_prod ? "production" : "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
-    plugins: [
-      
-    ],
-
+    plugins: plugins,
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
       ],
     },
   };
