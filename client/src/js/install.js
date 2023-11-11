a2ea01efc473714b5a8463f10e4ea9ed9de3784f
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-11 14:57:16 -0500

    Added and completed login in install.js...added comments throughout app to explain some of the functionality of the coding. App is complete and has been tested. README still needs to be updated, along with Heroku

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,48 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
+
+let deferredPrompt;
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the default prompt
+  event.preventDefault();
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  // Stash the event so it can be triggered later
+  deferredPrompt = event;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  // Show the install button
+  butInstall.style.display = "block";
+});
+
+// Click event handler for the install button
+butInstall.addEventListener("click", async () => {
+  if (deferredPrompt) {
+    // Simulate user interaction to trigger the installation
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the simulated prompt
+    const choiceResult = await deferredPrompt.userChoice;
+
+    // Check if the user accepted the simulated prompt
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the simulated install prompt");
+    } else {
+      console.log("User dismissed the simulated install prompt");
+    }
+
+    // Clear the deferredPrompt variable
+    deferredPrompt = null;
+
+    // Hide the install button
+    butInstall.style.display = "none";
+  }
+});
+
+// Event handler for appinstalled event
+window.addEventListener("appinstalled", (event) => {
+  // Log that the app was successfully installed
+  console.log("App successfully installed", event);
+
+  // Hide the install button
+  butInstall.style.display = "none";
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,13 @@
+// Importing necessary dependencies
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// check if process.env.NODE_ENV exist first
-// const is_prod = process.env.NODE_ENV && process.env.NODE_ENV.trim() === "production";
-
+// Environment Configuration: Check if NODE_ENV is set to "production"
 const is_prod = process.env.NODE_ENV.trim() === "production";
 
+// Array to hold webpack plugins
 const plugins = [
   new HtmlWebpackPlugin({
     template: "./index.html",
@@ -15,14 +15,15 @@ const plugins = [
   }),
 ];
 
+// If in production, add additional plugins for PWA and service worker
 if (is_prod) {
   plugins.push(
     ...[
+      // WebpackPwaManifest configuration for PWA manifest
       new WebpackPwaManifest({
         name: "Text Editor",
         short_name: "Txt Ed",
-        description:
-          "A text editor PWA (progressive web app) that can work both online and offline",
+        description: "A text editor PWA that can work both online and offline",
         background_color: "#ffffff",
         theme_color: "#000",
         publicPath: "/",
@@ -38,40 +39,43 @@ if (is_prod) {
   );
 }
 
+// Add Workbox InjectManifest plugin for service worker
 plugins.push(
   new InjectManifest({
-    swSrc: "./src-sw.js",
-    swDest: "service-worker.js",
+    swSrc: "./src-sw.js", // Path to the service worker source file
+    swDest: "service-worker.js", // Output filename for the service worker
   })
 );
 
+// Webpack configuration export
 module.exports = () => {
   return {
-    mode: is_prod ? "production" : "development",
+    mode: is_prod ? "production" : "development", // Set mode based on environment
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
+      main: "./src/js/index.js", // Entry point for the main bundle
+      install: "./src/js/install.js", // Entry point for the install bundle
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: "[name].bundle.js", // Output filename with [name] placeholder
+      path: path.resolve(__dirname, "dist"), // Output directory path
     },
-    plugins: plugins,
+    plugins: plugins, // Include the plugins array in the configuration
     module: {
       rules: [
+        // Webpack module rules for processing different file types can be added here
         {
-          test: /\.js$/,
+          test: /\.js$/, // Process JavaScript files
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: "babel-loader", // Use Babel for JavaScript transpilation
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ["@babel/preset-env"], // Babel preset for environment compatibility
             },
           },
         },
         {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          test: /\.css$/, // Process CSS files
+          use: ["style-loader", "css-loader"], // Use style-loader and css-loader
         },
       ],
     },
